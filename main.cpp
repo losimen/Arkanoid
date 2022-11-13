da989e02c2ef3c505b40b9f4a221ebfc823a5fbc
losimen <koval.pavlo03@gmail.com>
2022-11-13 11:16:03 +0200

    added command line arguments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include "Game.h"
 
 
-int main()
+int main(int argc,char* argv[])
 {
-    Game *game = new Game(800, 600);
+    int width = 800;
+    int height = 600;
+
+    if (argc == 3)
+    {
+        try
+        {
+            width = std::stoi(argv[1]);
+            height = std::stoi(argv[2]);
+        }
+        catch (std::invalid_argument)
+        {
+            std::cout << "Invalid arguments has to be integers" << std::endl;
+            return 1;
+        }
+    }
+
+    Game *game = new Game(width, height);
     return run(game);
 }
